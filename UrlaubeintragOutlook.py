e9d0d3f8fe41e39063b26600e8e6aa8b3fbad452
Dtakan <email@youremail.com>
2023-02-13 11:09:35 +0100

    Text change

--- UrlaubeintragOutlook.py
+++ UrlaubeintragOutlook.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 import datetime
 from tkinter import filedialog
 import win32com.client
+import win32com
 import pytz
 import time
 import pyperclip
@@ -21,34 +22,36 @@ class MainWindow:
         self.name_entry = tk.Entry(self.master)
         self.output_label = tk.Label(self.master, text='Wo soll die .txt Datei gespeichert werden?:')
         self.output_entry = tk.Entry(self.master)
-        self.browse_button = tk.Button(self.master, text='Durchsuchen', command=self.browse)
+        self.browse_button = tk.Button(self.master, text='Dateipfad Durchsuchen', command=self.browse)
         self.instructions = tk.Button(self.master, text='Anleitung', command=self.show_instructions)
+        self.plug = tk.Button(self.master, text='About', command=self.show_plug)
         self.information = tk.Button(self.master, text='Allgemeine Information', command=self.show_information)
         self.run_script = tk.Button(self.master, text='Skript starten', command=self.run_script)
         
-        self.name_label.grid(row=0, column=0, padx=5, pady=5, sticky='W')
-        self.name_entry.grid(row=0, column=1, padx=5, pady=5)
-        self.output_label.grid(row=1, column=0, padx=5, pady=5, sticky='W')
-        self.output_entry.grid(row=1, column=1, padx=5, pady=5)
-        self.browse_button.grid(row=1, column=2, padx=5, pady=5)
-        self.instructions.grid(row=2, column=0, padx=5, pady=5)
-        self.information.grid(row=2, column=1, padx=5, pady=5)
-        self.run_script.grid(row=2, column=2, padx=5, pady=5)
+        self.name_label.grid(row=1, column=0, padx=15, pady=5, sticky='W')
+        self.name_entry.grid(row=1, column=1, padx=5, pady=5)
+        self.output_label.grid(row=2, column=0, padx=15, pady=5, sticky='W')
+        self.output_entry.grid(row=2, column=1, padx=5, pady=5)
+        self.browse_button.grid(row=2, column=2, padx=5, pady=5)
+        self.instructions.grid(row=0, column=1, padx=5, pady=5)
+        self.plug.grid(row=0, column=3, padx=5, pady=5)
+        self.information.grid(row=0, column=0, padx=5, pady=5)
+        self.run_script.grid(row=3, column=3, padx=5, pady=5)
     
     def show_instructions(self):
-        messagebox.showinfo('Instructions', 'Sie müssen Outlook und die Seite mit Ihren Urlaubsdaten geöffnet haben:\n\n'
+        messagebox.showinfo('Instructions', 'Sie müssen Outlook und die Seite mit Ihren Urlaubsdaten (zB Zeus) geöffnet haben:\n\n'
                             '1. Geben Sie Ihren Nachnamen an.\n'
-                            '2. Wählen Sie aus wo die .txt Datei gespeichert werden soll\n'
+                            '2. Wählen Sie ein Dateipfad aus, in dem die .txt Datei gespeichert werden soll\n'
                             "3. Drücken Sie den 'Skript starten'-Button\n" 
                             '4. Sie haben danach 7 Sekunden um auf die Seite mit Ihren Urlaubsdaten zu wechseln\n'
-                            '5. Warten bis die Meldung erscheint, dass der Skript gelaufen ist\n'  
-                            "Das war's auch schon - Ihre Urlaubsdaten sind in Ihrem Outlook Kalender mit dem Betreff 'Urlaub IhrNachname' :-)")
+                            '5. Warten bis die Meldung erscheint, dass der Skript gelaufen ist\n \n'  
+                            "Das war's auch schon - Ihre Urlaubsdaten sind in Ihrem Outlook Kalender mit dem Betreff 'Urlaub IhrNachname' eingetragen :-)")
         
     def show_information(self):
-        messagebox.showinfo("Allgemeine Information", "Dieses Skript ist ein Bedienfeld-basiertes Tool, das es dem Benutzer ermöglicht, seine Urlaubsdaten in seinen Outlook-Kalender einzutragen. Der Benutzer muss lediglich seinen Namen eingeben und einen Speicherort auswählen, wo das Skript die erstellte .txt-Datei speichern soll. Durch Klicken auf den 'Skript starten' Button werden die Urlaubsdaten automatisch in den Outlook-Kalender eingetragen.")
+        messagebox.showinfo("Allgemeine Information", "Dieses Skript ist ein Bedienfeld-basiertes Tool, das es dem Benutzer ermöglicht, seine Urlaubsdaten in seinen Outlook-Kalender einzutragen. Der Benutzer muss lediglich seinen Namen eingeben und einen Speicherort auswählen, wo das Skript die erstellte .txt-Datei mit den Urlaubsdaten speichern soll (diese können später gelöscht werden, wenn gewünscht). Durch Klicken auf den 'Skript starten' Button werden die Urlaubsdaten automatisch in den Outlook-Kalender eingetragen.")
 
-def self_plug(self):
-    messagebox.showinfo("About", "Dieses Skript wurde erstellt von Atakan Taşdirek. Die Sourcecode können Sie in meinem Github Account einsehen (@Dtakan). ")
+    def show_plug(self):
+        messagebox.showinfo("About", "Dieses Skript wurde erstellt von Atakan Taşdirek. Die Sourcecode können Sie in meinem Github Account einsehen (@Dtakan).")
 
     def browse(self):
         folder_selected = filedialog.askdirectory()
@@ -114,10 +117,9 @@ def self_plug(self):
             start_datetime = pytz.utc.localize(datetime.datetime.combine(start_date, datetime.time(hour=1, minute=0, second=0)))
             appointment.Start = start_datetime
             appointment.End = datetime.datetime.combine(end_date, datetime.time(hour=23, minute=59, second=59))
-            appointment.Subject = 'Vacation ' + self.name_entry.get()
+            appointment.Subject = 'Urlaub ' + self.name_entry.get()
             appointment.BusyStatus = 3
             appointment.Save()
-        
         messagebox.showinfo('Erledigt!', 'Ihre Urlaubsdaten wurden in Outlook eingetragen und ihre Urlaubsdaten sind gespeichert in: ' + file_path)
 
 root = tk.Tk()
